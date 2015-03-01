fa9640cf851b2dcd8cfe46ab92267a12ae4b6005
Joel Mertanen <joel.mertanen@gmail.com>
2015-03-01 14:41:21 +0200

    implemented a simple AI

--- app/scripts/services/aiplayer.js
+++ app/scripts/services/aiplayer.js
@@ -19,6 +19,23 @@ angular.module('kivipeli')
 
     return service;
 
+    function isWinningCell(row, column) {
+      // the way to find a winning cell is just a generalized backwards induction
+      if (row === 1 && column % 2 === 1) {
+        return true;
+      }
+
+      if (column === 1 && row % 2 === 1) {
+        return true;
+      }
+
+      if (column % 2 === 0 || row % 2 === 0) {
+        return true;
+      }
+
+      return false;
+    }
+
     function wrapMakeMove(currentLocation) {
       var def = $q.defer();
 
@@ -40,6 +57,25 @@ angular.module('kivipeli')
         return 'left';
       }
 
+      // try to move to a losing cell to win the game.
+      // there can be always only one, so check them in order
+
+      // left
+      if (isWinningCell(currentLocation.row, currentLocation.column - 1)) {
+        return 'left';
+      }
+
+      // diagonal
+      if (isWinningCell(currentLocation.row - 1, currentLocation.column - 1)) {
+        return 'diagonal';
+      }
+
+      // up
+      if (isWinningCell(currentLocation.row - 1, currentLocation.column)) {
+        return 'up';
+      }
+
+      // no winning cells, just go somewhere
       var random = Math.random();
 
       if (random < 0.3) {
